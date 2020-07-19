c76a9904910edbc5ad9072ce99ad48a31ad1352f
natalie <natalie@enkin.co.il>
2020-07-19 15:01:51 +0300

    some changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,21 +16,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header" data-testid="header">
-<<<<<<< HEAD
-      <FontAwesomeIcon
-        icon={faBars}
-        className="header_ham"
-        onClick={setSideBarWidth}
-      />
-      <img src={yad} alt="logo" className="header_logo" />
-      <img src={userLogo} alt="userLogo" className="header_userLogo" />
-=======
       <FontAwesomeIcon icon={faBars} className="header_ham" />
       <img src={yad} alt="logo" className="header_logo" />
       <Link className="product_info_link" to={`/profile`}>
         <img src={userLogo} alt="userLogo" className="header_userLogo" />{" "}
       </Link>
->>>>>>> upstream/master
     </div>
   );
 };
